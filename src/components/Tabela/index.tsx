f7b947f0b9eb0bd7406c1caf9c1163e3a7b30b57
Paola K. <kloch.157@gmail.com>
2024-02-03 13:16:17 -0300

    .

--- src/components/Tabela/index.tsx
+++ src/components/Tabela/index.tsx
@@ -1,48 +1,67 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import styled from "@emotion/styled";
+import { Paper } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import IConsulta from "../../types/IConsulta";
 
+const CelulaEstilizada = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+        color: "var(--azul-escuro)",
+        fontSize: 18,
+        fontWeight: 700,
+        fontFamily: "var(--fonte-principal)"
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 16,
+        fontFamily: "var(--fonte-principal)"
+    }
+}))
+
+const LinhaEstilizada = styled(TableRow)(() => ({
+    [`&:nth-of-type(odd)`]: {
+        backgroundColor: "var(--cinza-claro)",
+        align: "right"
+    }
+}))
+
+
 function Tabela({ consultas }: { consultas: IConsulta[] | null }) {
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }} aria-labbel="tabela-customizada">
-          <TableHead>
-            <TableRow>
-              <TableCell>Data</TableCell>
-              <TableCell>Horário</TableCell>
-              <TableCell>Profissional</TableCell>
-              <TableCell>Especialidade</TableCell>
-              <TableCell>Paciente</TableCell>
-              <TableCell>Modalidade</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {consultas?.map((linha) => {
-              return (
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    {linha.data}
-                  </TableCell>
-                  <TableCell>{linha.horario}</TableCell>
-                  <TableCell>{linha.profissional[0].nome}</TableCell>
-                  <TableCell>{linha.profissional[0].especialidade}</TableCell>
-                  <TableCell>{linha.paciente}</TableCell>
-                  <TableCell>{linha.modalidade}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="tabela-customizada">
+                    <TableHead>
+                        <TableRow>
+                            <CelulaEstilizada>Data</CelulaEstilizada>
+                            <CelulaEstilizada>Horário</CelulaEstilizada>
+                            <CelulaEstilizada>Profissional</CelulaEstilizada>
+                            <CelulaEstilizada>Especialidade</CelulaEstilizada>
+                            <CelulaEstilizada>Paciente</CelulaEstilizada>
+                            <CelulaEstilizada>Modalidade</CelulaEstilizada>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {consultas?.map((linha) => {
+                            return (
+                                <LinhaEstilizada>
+                                    <CelulaEstilizada component="th" scope="row">{new Date(linha.data).toLocaleDateString()}</CelulaEstilizada>
+                                    <CelulaEstilizada>{linha.horario}</CelulaEstilizada>
+                                    <CelulaEstilizada>{linha.profissional[0].nome}</CelulaEstilizada>
+                                    <CelulaEstilizada>{linha.profissional[0].especialidade}</CelulaEstilizada>
+                                    <CelulaEstilizada>{linha.paciente}</CelulaEstilizada>
+                                    <CelulaEstilizada>{linha.modalidade}</CelulaEstilizada>
+                                </LinhaEstilizada>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
 }
+
 export default Tabela;
